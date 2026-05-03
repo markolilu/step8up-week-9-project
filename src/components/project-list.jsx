45b00623869f467a3990516f17de9a4ca98712e7
markolilu <marklumbpt@gmail.com>
2026-05-03 14:55:22 +0100

    put the rep[o list in the wrong place, oops. It is now in its own component

--- src/components/project-list.jsx
+++ src/components/project-list.jsx
@@ -1,1 +1,35 @@
 // use Github API to fetch and display a list of projects, including project name, description, and link to the repository
+
+import { useState, useEffect } from "react";
+
+function RepoList() {
+    const [repos, setRepos] = useState([]);
+    
+    useEffect(() => {
+        async function fetchRepos() {
+            try {
+
+            const response = await fetch('https://api.github.com/users/markolilu/repos');
+            const data = await response.json();
+            setRepos(data);
+            } catch (error) {
+                console.error('Error fetching repos:', error);
+            }
+
+        }
+        fetchRepos();
+    }, []);
+
+
+    return (
+        <div>
+            <h2>My GitHub Repositories</h2>
+            <ul>
+                {repos.map(repo => (
+                    <li key={repo.id}>{repo.name}</li>
+                ))}
+            </ul>
+        </div>
+    );
+                        
+}
